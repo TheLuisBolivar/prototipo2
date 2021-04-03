7ca7551667f30ad332d356fb75df9d4f4e905876
Rocioegrajales <rocioegrajales@gmail.com>
2021-04-02 20:13:38 -0500

    Botones de form y feedback funcionales

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import Search from "./components/Search";
 import listaContactos from "./contactos.json";
 import Button from '@material-ui/core/Button';
 import { Icon } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles ({
+  formButton: {    
+    width: 240,
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(156, 252, 183, .3)',
+    height: 48,
+    padding: '1%',
+    marginLeft: '16%',    
+  },
+
+  backButton: {
+    width:240, 
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(156, 252, 183, .3)',
+    color: 'white',
+    height: 48,
+    padding: '1%',
+    marginLeft: '16%',    
+    marginTop: '5%',
+    marginBottom: '5%'   
+  }
+})
 
 function App() {
+  const classes = useStyle()
+
   const [contactos, setContactos] = useState(listaContactos);
 
   const handleSearch = (e) => {
@@ -33,35 +61,30 @@ function App() {
         {contactos.map((contacto) => (
           <Contact contacto={contacto} key={contacto.id} />
         ))}
-      </div>
-      <div class="form-feedback">  
-        <Button      
-          color="inherint"
-          size= "large"
-          variant='secondary'
-          startIcon={<Icon>
-            manage_search
-          </Icon>}       
-          href='https://docs.google.com/forms/d/e/1FAIpQLScQJmsuWf9TVJwN9KThfqsN5I68m7FKcanrPJg1w5w54xrYrA/viewform?embedded=true'
-          >
-            Te lo encontramos
+      </div>      
+      
+      <div class="likefooter">
+        <Button className={classes.formButton}
+        startIcon={<Icon>
+          manage_search
+        </Icon>}
+        href='https://docs.google.com/forms/d/e/1FAIpQLScQJmsuWf9TVJwN9KThfqsN5I68m7FKcanrPJg1w5w54xrYrA/viewform?embedded=true'
+        target="_blank"
+        >
+          Te lo encontramos
         </Button>
 
-        <Button      
-          color="inherint"
-          size= "large"
-          variant='contained'
+        <Button className={classes.backButton}          
           startIcon={<Icon>
             feedback
           </Icon>}
           href="https://wa.me/573042846298"
-          >
-            Danos tu opinión
+          target="_blank"
+        >
+          Danos tu opinión 
         </Button>
-      </div>   
-      
-    </div>
-    
+      </div>      
+    </div>    
   );
 }
 
