ef5a52f85428756368d2dec261ffab9598ee4a1a
Rocioegrajales <rocioegrajales@gmail.com>
2021-04-09 17:20:17 -0500

    Serch bar, form/ feedback buttons fixed.

--- src/App.js
+++ src/App.js
@@ -2,37 +2,9 @@ import { useState } from "react";
 import Contact from "./components/Contact";
 import Search from "./components/Search";
 import listaContactos from "./contactos.json";
-import Button from '@material-ui/core/Button';
-import { Icon } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyle = makeStyles ({
-  formButton: {    
-    width: 240,
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(156, 252, 183, .3)',
-    height: 48,
-    padding: '1%',
-    marginLeft: '16%',    
-  },
-
-  backButton: {
-    width:240, 
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(156, 252, 183, .3)',
-    color: 'white',
-    height: 48,
-    padding: '1%',
-    marginLeft: '16%',    
-    marginTop: '5%',
-    marginBottom: '5%'   
-  }
-})
+import Footer from './components/Footer.js'
 
 function App() {
-  const classes = useStyle()
 
   const [contactos, setContactos] = useState(listaContactos);
 
@@ -61,30 +33,13 @@ function App() {
         {contactos.map((contacto) => (
           <Contact contacto={contacto} key={contacto.id} />
         ))}
-      </div>      
-      
-      <div class="likefooter">
-        <Button className={classes.formButton}
-        startIcon={<Icon>
-          manage_search
-        </Icon>}
-        href='https://docs.google.com/forms/d/e/1FAIpQLScQJmsuWf9TVJwN9KThfqsN5I68m7FKcanrPJg1w5w54xrYrA/viewform?embedded=true'
-        target="_blank"
-        >
-          Te lo encontramos
-        </Button>
+     </div>
+        <Footer/>          
+      </div>
 
-        <Button className={classes.backButton}          
-          startIcon={<Icon>
-            feedback
-          </Icon>}
-          href="https://wa.me/573042846298"
-          target="_blank"
-        >
-          Danos tu opinión 
-        </Button>
-      </div>      
-    </div>    
+
+
+      
   );
 }
 
